580162f3ce2110045e5eb2b20d17571544c475b8
abraham <abrahamcano1998@gmail.com>
2021-11-25 12:47:36 +0100

    install.txt update

---
+++ target/debug/deps/simple-569e6cef063a129c.d
@@ -0,0 +1,7 @@
+/home/abraham/Desktop/Projects/SupraOracles/BTC-USD-ws/target/debug/deps/simple-569e6cef063a129c: src/main.rs src/core.rs src/visualization.rs
+
+/home/abraham/Desktop/Projects/SupraOracles/BTC-USD-ws/target/debug/deps/simple-569e6cef063a129c.d: src/main.rs src/core.rs src/visualization.rs
+
+src/main.rs:
+src/core.rs:
+src/visualization.rs:
---
+++ target/debug/simple.d
@@ -0,0 +1,1 @@
+/home/abraham/Desktop/Projects/SupraOracles/BTC-USD-ws/target/debug/simple: /home/abraham/Desktop/Projects/SupraOracles/BTC-USD-ws/src/core.rs /home/abraham/Desktop/Projects/SupraOracles/BTC-USD-ws/src/main.rs /home/abraham/Desktop/Projects/SupraOracles/BTC-USD-ws/src/visualization.rs
